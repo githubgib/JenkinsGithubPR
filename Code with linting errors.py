5af824bfa33cf9d1807e252206f5cb4e28417466
githubgib <gibson.ikoro@gmail.com>
2024-03-06 01:16:42 +0000

    Update Code with linting errors.py

--- Code with linting errors.py
+++ Code with linting errors.py
@@ -2,4 +2,4 @@ def add_numbers(a, b):
     return a+b
 
 print(add_numbers(3, 4)) #good ax testinf=giuy  The lord is god
-print(add_numbers(5,6)) # Missing whitespace after comma testing Good Job
+print(add_numbers(5,6)) # Missing whitespace after comma testing Good Job There is error here
