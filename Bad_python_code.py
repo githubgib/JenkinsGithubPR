8de3a19a3a334d5749106e1f289076d20aaaeac3
githubgib <gibson.ikoro@gmail.com>
2024-03-06 10:18:50 +0000

    Update Bad_python_code.py

--- Bad_python_code.py
+++ Bad_python_code.py
@@ -2,8 +2,8 @@ def some_function():
  import os;os.system('ls') # Bad practice: semicolon, import inside function
 def anotherFunction(): # Non-PEP8 naming convention
   x = 1
-  if x == 1: # Unnecessary comparison codes
+  if x == 1: # Unnecessary comparison codes bad part
     print("X is 1")
   list = [1,2,3] # Shadowing built-in name 'list'
-  for i in range(len(list)): # Non-idiomatic loop
+  for i in range(len(list)): # Non-idiomatic loop 
     print(list[i])
