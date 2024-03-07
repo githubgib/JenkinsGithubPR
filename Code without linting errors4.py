dd55353cb57235e2fef208611dbaca16d799aa12
githubgib <gibson.ikoro@gmail.com>
2024-03-07 11:24:41 +0000

    Update Code without linting errors4.py

--- Code without linting errors4.py
+++ Code without linting errors4.py
@@ -1,5 +1,6 @@
 def add_numbers(a, b):
     return a + b
 
-print(add_numbers(3, 4))
-print(add_numbers(5, 6))
+
+print(add_numbers(3, 4))  # Add numbers 3 and 4
+print(add_numbers(5, 6))  # Add numbers 5 and 6
